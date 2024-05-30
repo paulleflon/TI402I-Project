3ff845e39232ca092fcb4299a9912abe0e409ad8
hickatheworld <cc@hicka.world>
2024-05-30 09:37:11 +0200

    feat(articles): add hydration

--- res/js/article.js
+++ res/js/article.js
@@ -1,3 +1,28 @@
+/** Hydrating page if data is found, displaying 404 if not */
+const id = new URL(location).searchParams.get('id');
+const data = reviews.find(e => e.id === id);
+if (!data) {
+	const notFound = document.createElement('div');
+	notFound.classList.add('not-found');
+	notFound.innerHTML = '<h1>404 - Not found</h1>' +
+		'<p>The article you are searching for doesn\'t</p>' +
+		'<p>You can go back <a href="index.html">Home</a> or <a href="aricles.html">check out our other articles</a>.</p>'
+	document.body.innerHTML = '';
+	document.body.appendChild(notFound);
+} else {
+	document.querySelector('h1').textContent = data.name;
+	document.querySelector('.header-rating span').textContent = data.grade;
+	document.querySelector('.header-park-infos #location span').textContent = data.location;
+	document.querySelector('.header-park-infos #area span').textContent = data.area + 'ha';
+	document.querySelector('.header-park-infos #walk span').textContent = data.duration + 'min';
+	document.querySelector('.article-header').style.backgroundImage = `url(${data.splashart})`;
+	document.querySelector('section.introduction').textContent = data.introduction;
+
+	const converter = new showdown.Converter();
+	document.querySelector('section.article').innerHTML = converter.makeHtml(data.article);
+}
+
+
 const headingElements = Array.from(document.querySelectorAll('h2,h3,h4'));
 
 /* Add ids for anchor links */
--- res/js/data.js
+++ res/js/data.js
@@ -5,9 +5,49 @@ const reviews = [
         grade: 5,
         location: 'Sceaux',
         area: 180,
+        duration: 120,
         splashart: 'res/img/Parc_de_Sceaux.jpg',
-        article: '## Article content in markdown.',
-        favorite: true
+        introduction: 'Located just a short journey from the bustling heart of Paris, Parc de Sceaux offers a serene escape with its sprawling lawns, beautifully landscaped gardens, and historical charm. This review will guide you through the park’s highlights and provide insights into why it’s a must-visit for anyone looking to experience a piece of French elegance and tranquility.',
+        favorite: true,
+        article: '## Parc de Sceaux: A Tranquil Retreat Near Paris\n' +
+            '\n' +
+            '### The Grandeur of Château de Sceaux\n' +
+            '\n' +
+            'At the center of the park stands the majestic Château de Sceaux, a stunning 17th-century mansion that now houses the Musée de l\'Île-de-France. The museum showcases a collection of art and artifacts that celebrate the history and culture of the Île-de-France region. The château itself is a marvel of architecture, with its elegant façade and well-preserved interiors offering a glimpse into the opulent lifestyles of the French aristocracy.\n' +
+            '\n' +
+            '### The Gardens: A Symphony of Nature and Art\n' +
+            '\n' +
+            '#### The French Gardens\n' +
+            '\n' +
+            'Designed by the renowned landscape architect André Le Nôtre, the French gardens at Parc de Sceaux are a masterpiece of symmetry and beauty. The meticulously manicured lawns, perfectly trimmed hedges, and vibrant flower beds create a picturesque setting. The grand canal and reflecting pools add to the park’s aesthetic appeal, offering visitors a chance to relax by the water and enjoy the scenic views.\n' +
+            '\n' +
+            '#### The Cherry Blossom Orchard\n' +
+            '\n' +
+            'One of the park’s most enchanting features is its cherry blossom orchard. During the spring, the orchard bursts into a riot of pink and white blossoms, attracting visitors from all over. It’s a perfect spot for a leisurely stroll or a peaceful picnic under the blooming trees.\n' +
+            '\n' +
+            '### Activities and Amenities\n' +
+            '\n' +
+            '#### Leisure and Recreation\n' +
+            '\n' +
+            'Parc de Sceaux offers a variety of recreational activities for visitors of all ages. The expansive lawns are ideal for picnics, games, and sunbathing. There are also designated areas for sports such as tennis and pétanque, providing ample opportunities for active pursuits.\n' +
+            '\n' +
+            '#### Cultural Events\n' +
+            '\n' +
+            'Throughout the year, the park hosts numerous cultural events, including classical music concerts, art exhibitions, and seasonal festivals. These events add a vibrant touch to the park’s serene ambiance and offer visitors a chance to immerse themselves in French culture.\n' +
+            '\n' +
+            '### Practical Information\n' +
+            '\n' +
+            '#### Accessibility\n' +
+            '\n' +
+            'The park is easily accessible from Paris by public transport. The RER B line takes you directly to the Parc de Sceaux station, from where it’s a short walk to the park’s entrance. There are also ample parking facilities for those driving to the park.\n' +
+            '\n' +
+            '#### Visitor Services\n' +
+            '\n' +
+            'The park is well-equipped with visitor amenities, including restrooms, drinking fountains, and a café where you can enjoy refreshments. There are also information boards and maps available at key points to help you navigate the park.\n' +
+            '\n' +
+            '### Conclusion\n' +
+            '\n' +
+            'Parc de Sceaux is a perfect blend of natural beauty, historical significance, and recreational opportunities. Whether you’re a history buff, a nature lover, or simply looking for a peaceful retreat from the city, this park has something to offer. Its well-maintained gardens, cultural attractions, and tranquil atmosphere make it a top destination for both locals and tourists alike. So next time you’re in Paris, set aside a day to explore this hidden gem and experience the timeless charm of Parc de Sceaux.',
     },
     {
         id: 'andre-citroen',
@@ -15,9 +55,55 @@ const reviews = [
         grade: 4.5,
         location: 'Paris',
         area: 14,
+        duration: 30,
         splashart: 'res/img/Parc_Andre_Citroen.jpg',
-        article: '## Article content in markdown.',
-        favorite: true
+        favorite: true,
+        introduction: 'Nestled in the 15th arrondissement of Paris, Parc André Citroën is a contemporary green space that offers a unique blend of innovative design and natural beauty. This review highlights the park\'s distinctive features and explains why it is a must-visit destination for anyone exploring the modern side of Paris.',
+        article: '## Parc André Citroën: A Modern Oasis in Paris\n' +
+            '\n' +
+            '### The Innovative Design\n' +
+            '\n' +
+            '#### A Modern Masterpiece\n' +
+            '\n' +
+            'Opened in 1992, Parc André Citroën is a departure from traditional French parks. Designed by architects Patrick Berger, Jean-François Jodry, and landscape architect Gilles Clément, the park spans 35 acres and is built on the site of the former Citroën automobile manufacturing plant. The park’s design seamlessly integrates modern architectural elements with lush greenery, creating a dynamic and engaging environment.\n' +
+            '\n' +
+            '#### The Themed Gardens\n' +
+            '\n' +
+            'The park features several themed gardens, each with its own distinct character. The "Jardin des Métamorphoses" (Garden of Metamorphosis) and "Jardin des Saisons" (Garden of Seasons) are particularly noteworthy. These gardens are designed to reflect the changing seasons and the evolution of plant life, offering visitors a constantly evolving landscape to enjoy throughout the year.\n' +
+            '\n' +
+            '### Key Attractions\n' +
+            '\n' +
+            '#### The Grande Serre\n' +
+            '\n' +
+            'One of the park\'s standout features is the Grande Serre, a large greenhouse that houses a variety of exotic plants and trees. This glass structure provides a tropical retreat within the park, allowing visitors to experience a diverse range of flora from different parts of the world.\n' +
+            '\n' +
+            '#### The Ballon de Paris\n' +
+            '\n' +
+            'The Ballon de Paris is another iconic attraction. This tethered hot air balloon offers visitors a unique opportunity to see Paris from above. Rising to a height of 150 meters, the balloon provides breathtaking panoramic views of the city, making it a highlight for both locals and tourists.\n' +
+            '\n' +
+            '### Activities and Amenities\n' +
+            '\n' +
+            '#### Leisure and Relaxation\n' +
+            '\n' +
+            'Parc André Citroën is designed with relaxation and recreation in mind. Expansive lawns invite visitors to lounge and picnic, while the various water features, including fountains and canals, add a soothing ambiance. There are also designated play areas for children, making it a family-friendly destination.\n' +
+            '\n' +
+            '#### Sports and Exercise\n' +
+            '\n' +
+            'For those looking to stay active, the park offers ample space for jogging, cycling, and other outdoor activities. The open areas and wide paths are perfect for a leisurely stroll or an invigorating run.\n' +
+            '\n' +
+            '### Practical Information\n' +
+            '\n' +
+            '#### Accessibility\n' +
+            '\n' +
+            'Parc André Citroën is easily accessible via public transport. The park is located near the Javel-André Citroën metro station (Line 10) and the RER C Javel station. There are also several bus lines that serve the area, making it convenient to reach from different parts of the city.\n' +
+            '\n' +
+            '#### Visitor Services\n' +
+            '\n' +
+            'The park is equipped with various amenities to enhance your visit. These include restrooms, drinking fountains, and several cafés where you can enjoy a snack or a meal. Information boards and maps are strategically placed to help you navigate the park and discover all its attractions.\n' +
+            '\n' +
+            '### Conclusion\n' +
+            '\n' +
+            'Parc André Citroën is a testament to modern landscape architecture, offering a refreshing alternative to traditional Parisian parks. Its innovative design, diverse gardens, and unique attractions make it a standout destination for both locals and visitors. Whether you\'re looking to relax, explore, or simply enjoy a day outdoors, Parc André Citroën provides a perfect setting to experience the contemporary charm of Paris.\n',
     },
     {
         id: 'hauts-de-bruyere',
@@ -25,6 +111,7 @@ const reviews = [
         grade: 4,
         location: 'Villejuif',
         area: 20,
+        duration: 80,
         splashart: 'res/img/Parc_Departemental_Hauts_de_Bruyeres.jpg',
         article: '## Article content in markdown.',
         favorite: true
@@ -35,6 +122,7 @@ const reviews = [
         grade: 4.2,
         location: 'Nanterre',
         area: 25,
+        duration: 90,
         splashart: 'res/img/Parc_Andre_Malraux.jpg',
         article: '## Article content in markdown.',
         favorite: true
@@ -45,6 +133,7 @@ const reviews = [
         grade: 4.6,
         location: 'Paris',
         area: 15,
+        duration: 80,
         splashart: 'res/img/Parc_Montsouris.webp',
         article: '## Article content in markdown.',
         favorite: true
@@ -55,6 +144,7 @@ const reviews = [
         grade: 4.3,
         location: 'Cachan',
         area: 8,
+        duration: 30,
         splashart: 'res/img/Parc_Raspail.jpg',
         article: '## Article content in markdown.',
         favorite: true
